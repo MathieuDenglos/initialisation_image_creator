bb778550918426188b7c102e3e6f5181f629c88e
MathieuDenglos <matdenglos@gmail.com>
2021-09-20 21:19:34 +0200

    deal with settings page layout

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.layouts import general_layout as gl
+from src.layouts import page_rb_layout as rbl
 
 
 def main():
-    gl.general_layout()
-    gl.detailed_layout()
+    # gl.general_layout()
+    # gl.detailed_layout()
 
+    rbl.page_rb1()
 
 if __name__ == "__main__":
     main()
