84a34f0dcecc3bc81fc55c2b6813222174c79632
MathieuDenglos <matdenglos@gmail.com>
2021-09-22 22:21:43 +0200

    add possibility to choose image size

--- src/default/image.py
+++ src/default/image.py
@@ -8,19 +8,23 @@ class ImageDrawer:
     image = None
     drawing = None
 
-    def __init__(self, image_name):
+    def __init__(self, image_name, width=640, height=480):
         """ Permet d'initialiser une nouvelle image
 
         Parameters
         ----------
         image_name: `str`
             Le nom de l'image avec son extension (généralement .png). Celle ci sera enregistrée dans le répertoire image
+        width: `int`
+            Longueur de l'image créée
+        height: `int`
+            Hauteur de l'image créée
         """
         # Enregistre le nom de l'image pour la sauvegarder plus tard
         self.image_name += image_name if len(image_name.split(".")) >= 2 else image_name + ".png"
 
         # Initialise l'image
-        self.image = Image.new("RGB", (640, 480), (3, 17, 34))
+        self.image = Image.new("RGB", (width, height), (3, 17, 34))
         self.drawing = ImageDraw.Draw(self.image)
 
     def draw(self, shapes):
